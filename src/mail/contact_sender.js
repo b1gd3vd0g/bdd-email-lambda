502d4364289b2a7e329048234303e2a76b4646d8
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-17 16:11:04 -0700

    Export a function which attempts to send the contact form email. It takes in the relevant data as parameters, and returns an ApiResponse object based on the status of the request.

---
+++ src/mail/contact_sender.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const transporter = require('./transporter');
+const { ApiResponse } = require('../util/api_response');
+
+/**
+ * Attempt to send an email to Devin (`devin@bigdevdog.com`) containing the
+ * following information:
+ * @param {string} name The name of the sender.
+ * @param {string} email The email address of the sender.
+ * @param {string} phone The phone number of the sender.
+ * @param {string} header The header/subject line of the message.
+ * @param {string} message The body of the message.
+ * @return {ApiResponse}
+ */
+exports.sendContactFormEmail = async function (
+  name,
+  email,
+  phone,
+  header,
+  message
+) {
+  /** Fill in a template with the proper data. */
+  function fillInTemplate(template) {
+    return template
+      .replace(/~~NAME~~/g, name)
+      .replace(/~~EMAIL~~/g, email)
+      .replace(/~~PHONE~~/g, phone)
+      .replace(/~~HEADER~~/g, header)
+      .replace(/~~MESSAGE~~/g, message);
+  }
+
+  const templatePath = '../templates/cf_message';
+  const htmlTemplate = fs.readFileSync(`${templatePath}.html`).toString();
+  const txtTemplate = fs.readFileSync(`${templatePath}.txt`).toString();
+  const html = fillInTemplate(htmlTemplate);
+  const text = fillInTemplate(txtTemplate);
+
+  try {
+    const response = await transporter.sendMail({
+      html,
+      text,
+      from: '"Big Dev Dog Automailer" <automailer@bigdevdog.com>',
+      to: 'devin@bigdevdog.com',
+      subject: `${name} wants to get in touch!`
+    });
+
+    if (response.accepted.length) {
+      return new ApiResponse(
+        200,
+        'Your message has been sent successfully. Please allow up to two days for a response.'
+      );
+    } else {
+      return new ApiResponse(
+        500,
+        'Something went wrong, and Devin could not receive your message.',
+        { info: response }
+      );
+    }
+  } catch (error) {
+    return new ApiResponse(
+      500,
+      'Your message could not be sent. Try again later.',
+      { error }
+    );
+  }
+};
