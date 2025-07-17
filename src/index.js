7d13ac1eca8b5a1048bcbaf9380db295e25a7e1b
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-17 16:14:40 -0700

    Fix handler function to read the lambda event properly.  This module is currently trying to handle CORS as well, which is leading to some funky behavior.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 const sendContactFormEmail = require('./handlers/contact_form');
 
-exports.handler = async (event) => {
-  const { httpMethod, path, body } = event;
-  const parsedBody = JSON.parse(body);
-  if (httpMethod === 'POST') {
-    // They want to send an email.
+const ALLOWED_ORIGINS = ['https://bigdevdog.com', 'https://www.bigdevdog.com'];
+const ALLOWED_METHODS = ['OPTIONS', 'POST'];
+const ALLOWED_HEADERS = ['Content-Type'];
 
-    if (path === '/contact') {
-      // They want to send an email from the contact form at
-      // https://www.bigdevdog.com/contact.
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': ALLOWED_ORIGINS,
+  'Access-Control-Allow-Methods': ALLOWED_METHODS,
+  'Access-Control-Allow-Headers': ALLOWED_HEADERS
+};
+
+// TODO: deal ONLY with routes and handlers here. Move handlers for specific
+// routes to separate files. Move the email-specific functionality separate to
+// that.
+
+exports.handler = async (event) => {
+  const { routeKey, body, headers } = event;
+  const [method, path] = routeKey.split(' ');
+  console.log({ method, path });
+  console.log({ headers });
+  if (path === '/contact') {
+    if (method === 'OPTIONS') {
+      const originAllowed = ALLOWED_ORIGINS.includes(headers.origin);
+      return {
+        statusCode: originAllowed ? 204 : 403,
+        headers: CORS_HEADERS,
+        body: ''
+      };
+    }
+
+    if (method === 'POST') {
+      const parsedBody = JSON.parse(body);
       const { name, email, phone, header, message } = parsedBody;
-      const result = await sendContactFormEmail(
+      const { status, info } = await sendContactFormEmail(
         name,
         email,
         phone,
@@ -18,10 +40,9 @@ exports.handler = async (event) => {
         message
       );
       return {
-        statusCode: 200,
-        body: JSON.stringify({
-          result
-        })
+        statusCode: status,
+        headers: CORS_HEADERS,
+        body: JSON.stringify(info)
       };
     }
   }
